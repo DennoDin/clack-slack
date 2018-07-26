4eed912210a80fe7935166468797c82e18281779
Code Chrysalis <codechrysalis@CC-Mac-Mini-4.local>
2018-07-26 17:56:52 +0900

    completed create channels and list channels
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Nour Balaha <nourbalaha0@gmail.com>

--- services/db/channels/create.js
+++ services/db/channels/create.js
@@ -1,8 +1,29 @@
+const Promise = require("bluebird");
 const validateChannelName = (uName) =>
   typeof uName === "string" && uName.replace(" ", "").length > 3;
 
 module.exports = (knex, Channel) => {
   return (params) => {
-    return Promise.resolve({}); // fix me!
+    const channelName = params.name;
+
+    return Promise.try(() => {
+      if (!validateChannelName(channelName))
+        throw new Error(
+          "Channel Name must be provided, and be at least three characters"
+        );
+    })
+      .then(() => knex("channels").insert({ name: channelName.toLowerCase() }))
+      .then(() => {
+        return knex("channels")
+          .where({ name: channelName.toLowerCase() })
+          .select();
+      })
+      .then((channels) => new Channel(channels.pop()))
+      .catch((err) => {
+        if (err.message.match("duplicate key value"))
+          throw new Error("That channel already exists");
+
+        throw err;
+      });
   };
 };
