fc15becb2593584828ab59cd21bb3c7b57cb0bcf
Code Chrysalis <codechrysalis@CC-Mac-Mini-4.local>
2018-07-27 14:39:51 +0900

    finished create channelMessages
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Nour Balaha <nourbalaha0@gmail.com>

--- services/db/channelMessages/create.js
+++ services/db/channelMessages/create.js
@@ -1,25 +1,40 @@
 const Promise = require("bluebird");
 const validateMessage = (uMessage) =>
-  typeof uMessage === "string" && uMessage.replace(" ", "").length > 3;
+  typeof uMessage === "string" && uMessage.replace(" ", "").length > 0;
 
-module.exports = (knex, channelMessage) => {
+module.exports = (knex, ChannelMessage) => {
   return ({ fromId, channelId, message }) => {
     return Promise.try(() => {
       if (!validateMessage(message)) {
         throw new Error(
-          "Channel Name must be provided, and be at least three characters"
+          "Channel Message must be provided, and be at least 1 characters"
         );
       }
     })
       .then(() =>
-        knex("channel_messages").insert({ name: message.toLowerCase() })
+        knex("channel_messages").insert({
+          channel_id: channelId,
+          from_id: fromId,
+          message,
+        })
       )
       .then(() => {
         return knex("channel_messages")
-          .where({ name: message.toLowerCase() })
+          .innerJoin("users", "from_id", "=", "users.id")
+          .innerJoin("channels", "channel_id", "=", "channels.id")
           .select();
       })
-      .then((channels) => new Channel(channels.pop()))
+      .then((cMessage) => {
+        const obj = {
+          from: cMessage[0].username,
+          to: cMessage[0].name,
+          message: cMessage[0].message,
+          id: cMessage[0].id,
+          sent_at: cMessage[0].sent_at,
+        };
+        return [new ChannelMessage(obj)];
+      })
+
       .catch((err) => {
         if (err.message.match("duplicate key value"))
           throw new Error("That channel already exists");
