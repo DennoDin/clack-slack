3058c96bee9d051c73ea262744501bd8d1b05305
Code Chrysalis <codechrysalis@CC-Mac-Mini-4.local>
2018-07-27 15:17:39 +0900

    Finished channelMessages list function and added bluebird to the tests.
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Nour Balaha <nourbalaha0@gmail.com>

--- services/db/channelMessages/list.js
+++ services/db/channelMessages/list.js
@@ -1,1 +1,30 @@
-module.exports = (knex, ChannelMessage) => (params) => Promise.resolve([]);
+// module.exports = (knex, ChannelMessage) => (params) => Promise.resolve([]);
+const Promise = require("bluebird");
+
+module.exports = (knex, ChannelMessage) => {
+  return ({ channelId }) => {
+    return Promise.try(() => {})
+      .then(() => {
+        return knex("channel_messages")
+          .innerJoin("users", "from_id", "=", "users.id")
+          .innerJoin("channels", "channel_id", "=", "channels.id")
+          .where("channels.id", channelId)
+          .select();
+      })
+      .then((cMessage) => {
+        return cMessage.map((singleMessage) => {
+          const obj = {
+            from: singleMessage.username,
+            to: singleMessage.name,
+            message: singleMessage.message,
+            id: singleMessage.id,
+            sent_at: singleMessage.sent_at,
+          };
+          return new ChannelMessage(obj);
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+};
